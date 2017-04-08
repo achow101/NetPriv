cc6ebcad197b4a7afdc1b532a9a21bf0b2e68a24
Andrew Chow <achow101@gmail.com>
2017-04-08 19:35:05 -0400

    Proper sorting of tables; better variable names

--- overview.cpp
+++ overview.cpp
@@ -7,8 +7,8 @@
 #include <QTimer>
 
 QString temp = "hue", a = "";
-QMap<QString, int> test;
-QMap<QString, int> programs;
+QMap<QString, int> hostname_data;
+QMap<QString, int> prog_data;
 int i, test2 = 100, test3 = 1000, row_index = 0;
 
 Overview::Overview(QWidget *parent) :
@@ -57,32 +57,39 @@ Overview::~Overview()
 
 void Overview::auto_update_traffic()
 {
-    QStandardItemModel *model = new QStandardItemModel(1, 2, this);
+    // Traffic table
+    QStandardItemModel *traffic_model = new QStandardItemModel(1, 2, this);
 
-    model->setHorizontalHeaderItem(0, new QStandardItem(QString("IP Address")));
-    model->setHorizontalHeaderItem(1, new QStandardItem(QString("Number of Packets Sent")));
-    test = get_hostnames_list();
-    for (QString ip: test.keys()) {
-            model->setVerticalHeaderItem(row_index, new QStandardItem(QString::number(row_index + 1)));
-            model->setItem(row_index, 0, new QStandardItem(ip));
-            model->setItem(row_index, 1, new QStandardItem(QString::number(test.value(ip))));
+    traffic_model->setHorizontalHeaderItem(0, new QStandardItem(QString("IP Address")));
+    traffic_model->setHorizontalHeaderItem(1, new QStandardItem(QString("Number of Packets Sent")));
+    hostname_data = get_hostnames_list();
+    for (QString ip: hostname_data.keys()) {
+            traffic_model->setVerticalHeaderItem(row_index, new QStandardItem(QString::number(row_index + 1)));
+            traffic_model->setItem(row_index, 0, new QStandardItem(ip));
+            QStandardItem *number_item = new QStandardItem();
+            number_item->setData(QVariant(hostname_data.value(ip)), Qt::DisplayRole);
+            traffic_model->setItem(row_index, 1, number_item);
             row_index++;
     }
     row_index = 0;
-    ui->tableView->setModel(model);
+    traffic_model->sort(1, Qt::DescendingOrder);
+    ui->tableView->setModel(traffic_model);
 
-    //second one
-    QStandardItemModel *model2 = new QStandardItemModel(1, 2, this);
+    // Programs table
+    QStandardItemModel *prog_model = new QStandardItemModel(1, 2, this);
 
-    model2->setHorizontalHeaderItem(0, new QStandardItem(QString("Program Name")));
-    model2->setHorizontalHeaderItem(1, new QStandardItem(QString("Number of Packets Sent")));
-    programs = get_programs_map();
-    for (QString program_name: programs.keys()) {
-            model2->setVerticalHeaderItem(row_index, new QStandardItem(QString::number(row_index + 1)));
-            model2->setItem(row_index, 0, new QStandardItem(program_name));
-            model2->setItem(row_index, 1, new QStandardItem(QString::number(programs.value(program_name))));
+    prog_model->setHorizontalHeaderItem(0, new QStandardItem(QString("Program Name")));
+    prog_model->setHorizontalHeaderItem(1, new QStandardItem(QString("Number of Packets Sent")));
+    prog_data = get_programs_map();
+    for (QString program_name: prog_data.keys()) {
+            prog_model->setVerticalHeaderItem(row_index, new QStandardItem(QString::number(row_index + 1)));
+            prog_model->setItem(row_index, 0, new QStandardItem(program_name));
+            QStandardItem *number_item = new QStandardItem();
+            number_item->setData(QVariant(prog_data.value(program_name)), Qt::DisplayRole);
+            prog_model->setItem(row_index, 1, number_item);
             row_index++;
     }
     row_index = 0;
-    ui->tableView_2->setModel(model2);
+    prog_model->sort(1, Qt::DescendingOrder);
+    ui->tableView_2->setModel(prog_model);
 }
